5ef59b96c83575f637be9a6d0f65f2d31eddf96a
John Portin <john.portin@proton.me>
2024-11-14 19:46:40 -0800

    Add: experiment runner for running many experiments at once. Rename montyhall.sh to Program.sh to comply with project requirements.

--- Library.sh
+++ Library.sh
@@ -11,3 +11,14 @@ logStatistics() {
 cleanUp() {
     rm -f "data.txt"
 }
+
+experimentRunner() {
+    if [[ -z "$1" ]]; then
+        echo "Requires one position argument, <numExperiments>"
+        echo "Usage: experimentRunner <numExperiments>"
+        exit 1
+    fi
+    numExperiments="$1"
+
+
+}
